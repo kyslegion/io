e28104846176cec7c87e003c5097672f6c4aa974
kyslegion <kyslegion@gmail.com>
2022-10-04 17:05:40 +0200

    oki

---
+++ index.js
@@ -0,0 +1,29 @@
+
+let port=4000;
+let x =__dirname;
+let express = require('express');
+let app = express();
+let path =require('path');
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, './views'))
+
+
+io.on("connection",(socket)=>{
+    socket.on('event',(event)=>{
+        console.log(event);
+        // socket.emit('three',({products}))
+    });
+});
+
+
+app.get('/',(req,res)=>{
+    res.render('index', {
+    });
+});
+
+http.listen(port, '0.0.0.0',()=>{
+  console.log(port);
+});
